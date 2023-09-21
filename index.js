dd2fb0ee30cc6f8932adafde667005a25896304b
SushankSinha <SushankSinha>
2023-09-21 21:32:14 +0530

    url update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => console.log("Server starting on port", PORT));
 
 app.use(
   cors({
-    origin: ["https://library-management-system.netlify.app"],
+    origin: ["https://ss-library-management-system.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
